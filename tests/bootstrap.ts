ea89463dfa9eca942727a3ca81cc0c0c59f06fd6
bmgla <bmg.laurent@live.fr>
2025-12-20 18:15:43 +0100

    feat: add unit test for authentication

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -32,6 +32,8 @@ export const runnerHooks: Required<Pick<Config, 'setup' | 'teardown'>> = {
  */
 export const configureSuite: Config['configureSuite'] = (suite) => {
   if (['browser', 'functional', 'e2e'].includes(suite.name)) {
-    return suite.setup(() => testUtils.httpServer().start())
+    return suite.setup(async () => {
+      await testUtils.httpServer().start()
+    })
   }
 }
