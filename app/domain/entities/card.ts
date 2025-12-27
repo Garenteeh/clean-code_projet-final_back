9e8c8a91c188666ec4f313b17b63ebc110937d25
Sagbyy <sbouhdjeur.gagny@gmail.com>
2025-12-27 19:08:01 +0100

    refactor: remove unused DTOs and use cases, and introduce Card entity

---
+++ app/domain/entities/card.ts
@@ -0,0 +1,9 @@
+export class Card {
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly description: string,
+    public readonly createdAt: Date,
+    public readonly updatedAt: Date
+  ) {}
+}
