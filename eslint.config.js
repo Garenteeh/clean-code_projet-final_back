7d28a48a04d19a060bad749ee62085dc30f83b1f
Sagbyy <sbouhdjeur.gagny@gmail.com>
2025-12-27 19:46:25 +0100

    chore: update ESLint configuration and add new AdonisJS dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,14 @@
 import { configApp } from '@adonisjs/eslint-config'
-export default configApp()
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default configApp({
+  languageOptions: {
+    parserOptions: {
+      tsconfigRootDir: resolve(__dirname),
+    },
+  },
+})
