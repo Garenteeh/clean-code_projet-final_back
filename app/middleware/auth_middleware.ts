b70c028f3d8cf8727b9176a76db919b0989c80ec
bmgla <bmg.laurent@live.fr>
2025-12-27 18:49:44 +0100

    feat: add auth_middleware and auth service implementation

--- app/middleware/auth_middleware.ts
+++ app/middleware/auth_middleware.ts
@@ -1,1 +1,10 @@
+import { HttpContext } from '@adonisjs/core/http'
+import { AuthService } from '#application/services/AuthService'
 
+const authService = new AuthService()
+
+export default async function authMiddleware({ request }: HttpContext, next: () => Promise<void>) {
+  const token = request.header('Authorization')
+  request.user = await authService.getUserFromToken(token)
+  await next()
+}
